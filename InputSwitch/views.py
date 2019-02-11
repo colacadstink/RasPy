f1a95a762f6f6f6fda211f826d3b526612684f24
Ben Harris <benharris5@gmail.com>
2019-02-11 00:37:21 -0500

    Tweak to input switch write

--- InputSwitch/views.py
+++ InputSwitch/views.py
@@ -7,6 +7,6 @@ def input_switch(request):
     input = request.GET.get('input', "+")
 
     with serial.Serial('/dev/ttyUSB0', 19200) as ser:
-        ser.write(bytes('sw '+input+'\n', 'ascii'))
+        ser.write(bytes('sw '+input+'\r\n', 'ascii'))
 
     return HttpResponse(status=204)
